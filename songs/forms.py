3546ff55bd39c298c95c5f46ed605e95809fd4cf
fabioBaps <fabiox2509@gmail.com>
2023-11-17 20:39:59 -0300

    CRUD com Django forms

---
+++ songs/forms.py
@@ -0,0 +1,34 @@
+from django.forms import ModelForm
+from .models import Post #, Comment
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = [
+            'title',
+            'artist',
+            'album',
+            'year',
+            'cover',
+            'lyrics',
+        ]
+        labels = {
+            'title': 'Título',
+            'artist': 'Artista',
+            'album': 'Álbum',
+            'year': 'Ano',
+            'cover': 'Capa',
+            'lyrics': 'Letra',
+        }
+        
+# class CommentForm(ModelForm):
+#     class Meta:
+#         model = Comment
+#         fields = [
+#             'author',
+#             'text',
+#         ]
+#         labels = {
+#             'author': 'Usuário',
+#             'text': 'Comentário',
+#         }
--- songs/views.py
+++ songs/views.py
@@ -1,94 +1,94 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from .models import Post #, Comment, Category
-# from .forms import PostForm, CommentForm
-from django.urls import reverse #, reverse_lazy
-#from django.views import generic
+from .forms import PostForm #, CommentForm
+from django.urls import reverse, reverse_lazy
+from django.views import generic
     
     
     
-def ListView(request):
-    post_list = Post.objects.all()
-    return render(request, 'songs/list.html', {'post_list': post_list})
+# def ListView(request):
+#     post_list = Post.objects.all()
+#     return render(request, 'songs/list.html', {'post_list': post_list})
 
-def DetailView(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
-    return render(request, 'songs/detail.html', {'post': post})
+# def DetailView(request, post_id):
+#     post = get_object_or_404(Post, pk=post_id)
+#     return render(request, 'songs/detail.html', {'post': post})
 
-def CreateView(request):
-    if request.method == 'POST':
-        post_title = request.POST['title']
-        post_artist = request.POST['artist']
-        post_album = request.POST['album']
-        post_year = request.POST['year']
-        post_cover = request.POST['cover']
-        post_lyrics = request.POST['lyrics']
-        post = Post(title=post_title,
-                      artist=post_artist,
-                      album=post_album,
-                      year=post_year,
-                      cover=post_cover,
-                      lyrics=post_lyrics)
-        post.save()
-        return HttpResponseRedirect(
-            reverse('songs:detail', args=(post.id, )))
-    else:
-        return render(request, 'songs/create.html', {})
+# def CreateView(request):
+#     if request.method == 'POST':
+#         post_title = request.POST['title']
+#         post_artist = request.POST['artist']
+#         post_album = request.POST['album']
+#         post_year = request.POST['year']
+#         post_cover = request.POST['cover']
+#         post_lyrics = request.POST['lyrics']
+#         post = Post(title=post_title,
+#                       artist=post_artist,
+#                       album=post_album,
+#                       year=post_year,
+#                       cover=post_cover,
+#                       lyrics=post_lyrics)
+#         post.save()
+#         return HttpResponseRedirect(
+#             reverse('songs:detail', args=(post.id, )))
+#     else:
+#         return render(request, 'songs/create.html', {})
     
-def UpdateView(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
+# def UpdateView(request, post_id):
+#     post = get_object_or_404(Post, pk=post_id)
 
-    if request.method == "POST":
-        post.title = request.POST['title']
-        post.artist = request.POST['artist']
-        post.album = request.POST['album']
-        post.year = request.POST['year']
-        post.cover = request.POST['cover']
-        post.lyrics = request.POST['lyrics']
-        post.save()
-        return HttpResponseRedirect(
-            reverse('songs:detail', args=(post.id, )))
+#     if request.method == "POST":
+#         post.title = request.POST['title']
+#         post.artist = request.POST['artist']
+#         post.album = request.POST['album']
+#         post.year = request.POST['year']
+#         post.cover = request.POST['cover']
+#         post.lyrics = request.POST['lyrics']
+#         post.save()
+#         return HttpResponseRedirect(
+#             reverse('songs:detail', args=(post.id, )))
 
-    context = {'post': post}
-    return render(request, 'songs/update.html', context)
+#     context = {'post': post}
+#     return render(request, 'songs/update.html', context)
 
-def DeleteView(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
+# def DeleteView(request, post_id):
+#     post = get_object_or_404(Post, pk=post_id)
 
-    if request.method == "POST":
-        post.delete()
-        return HttpResponseRedirect(reverse('songs:list'))
+#     if request.method == "POST":
+#         post.delete()
+#         return HttpResponseRedirect(reverse('songs:list'))
 
-    context = {'post': post}
-    return render(request, 'songs/delete.html', context)
+#     context = {'post': post}
+#     return render(request, 'songs/delete.html', context)
 
-# class postFormListView(generic.ListView):
-#     model = Post
-#     template_name = 'songs/list.html'
+class postFormListView(generic.ListView):
+    model = Post
+    template_name = 'songs/list.html'
 
     
-# class postFormDetailView(generic.DetailView):
-#     model = Post
-#     template_name = 'songs/detail.html'
+class postFormDetailView(generic.DetailView):
+    model = Post
+    template_name = 'songs/detail.html'
 
-# class postFormCreateView(generic.CreateView):
-#     model = Post
-#     template_name = 'songs/create.html'
-#     fields = ['title', 'artist', 'album', 'year', 'cover', 'lyrics']
-#     def get_success_url(self):
-#         return reverse('songs:detail', kwargs={'pk': self.object.pk})
+class postFormCreateView(generic.CreateView):
+    model = Post
+    template_name = 'songs/create.html'
+    fields = ['title', 'artist', 'album', 'year', 'cover', 'lyrics']
+    def get_success_url(self):
+        return reverse('songs:detail', kwargs={'pk': self.object.pk})
 
-# class postFormUpdateView(generic.UpdateView):
-#     model = Post
-#     form_class = PostForm
-#     template_name = 'songs/update.html'
-#     def get_success_url(self):
-#         return reverse('songs:detail', kwargs={'pk': self.object.pk})
+class postFormUpdateView(generic.UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'songs/update.html'
+    def get_success_url(self):
+        return reverse('songs:detail', kwargs={'pk': self.object.pk})
 
-# class postFormDeleteView(generic.DeleteView):
-#     model = Post
-#     template_name = 'songs/delete.html'
-#     success_url = reverse_lazy('songs:list')
+class postFormDeleteView(generic.DeleteView):
+    model = Post
+    template_name = 'songs/delete.html'
+    success_url = reverse_lazy('songs:list')
     
 
 # class postListView(generic.ListView):
